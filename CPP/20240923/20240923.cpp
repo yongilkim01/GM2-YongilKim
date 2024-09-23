759f90f5ef902a6945bcc4bece698a754a080e14
yongilkim01 <yongil.k01@gmail.com>
2024-09-23 14:21:50 +0900

    김용일: 첫번째 숙제(strlen 모방 함수 구현)

---
+++ CPP/20240923/20240923.cpp
@@ -0,0 +1,24 @@
+﻿#include <iostream>
+
+int StringCount(const char* const inStr)
+{
+    int strCount = 0;
+    while (inStr[strCount])
+    {
+        strCount++;
+        if (inStr[strCount] == '\0') break;
+    }
+
+    return strCount;
+}
+
+int main()
+{
+    const char* testStr = "AAAAAAAAAA";
+    int strSize = StringCount(testStr);
+
+    std::cout << "String size using StringCount : " << strSize << std::endl;
+    std::cout << "String size using strlen : " << strlen(testStr) << std::endl;
+
+    return 0;
+}
